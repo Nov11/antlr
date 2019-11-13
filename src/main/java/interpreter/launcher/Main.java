9162e88ea88e6fd355b17c185d14cbb3b49244de
nov11 <529079634@qq.com>
2019-11-13 12:37:05 +0800

    walker

--- src/main/java/interpreter/launcher/Main.java
+++ src/main/java/interpreter/launcher/Main.java
@@ -1,17 +1,69 @@
 package interpreter.launcher;
 
+import com.khubla.antlr4example.InterpreterLexer;
+import com.khubla.antlr4example.InterpreterListener;
+import com.khubla.antlr4example.InterpreterParser;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
-import interpreter.InterpreterParser;
-import interpreter.InterpreterLexer;
+import org.antlr.v4.runtime.ParserRuleContext;
+import org.antlr.v4.runtime.tree.ErrorNode;
+import org.antlr.v4.runtime.tree.IterativeParseTreeWalker;
+import org.antlr.v4.runtime.tree.TerminalNode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 public class Main {
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
         InterpreterLexer lexer = new InterpreterLexer(CharStreams.fromString("1+2+5"));
         InterpreterParser parser = new InterpreterParser(new CommonTokenStream(lexer));
 
-        parser.start();
+        InterpreterParser.StartContext ctx = parser.start();
+
+        IterativeParseTreeWalker walker = new IterativeParseTreeWalker();
+        walker.walk(new InterpreterListener() {
+            @Override
+            public void visitTerminal(TerminalNode terminalNode) {
+                logger.info("t : {}", terminalNode);
+            }
+
+            @Override
+            public void visitErrorNode(ErrorNode errorNode) {
+                logger.info("e: {}", errorNode);
+            }
+
+            @Override
+            public void enterEveryRule(ParserRuleContext parserRuleContext) {
+                logger.info("enterEveryRule: {}", parserRuleContext);
+            }
+
+            @Override
+            public void exitEveryRule(ParserRuleContext parserRuleContext) {
+                logger.info("exitEveryRule: {}", parserRuleContext);
+            }
+
+            @Override
+            public void enterStart(InterpreterParser.StartContext ctx) {
+                logger.info("enter s: {}", ctx);
+            }
+
+            @Override
+            public void exitStart(InterpreterParser.StartContext ctx) {
+                logger.info("exit s: {}", ctx);
+            }
+
+            @Override
+            public void enterExpression(InterpreterParser.ExpressionContext ctx) {
+                logger.info("enterExpression: {}", ctx);
+            }
+
+            @Override
+            public void exitExpression(InterpreterParser.ExpressionContext ctx) {
+                logger.info("exitExpression: {}", ctx);
+            }
+        }, ctx);
         System.out.println("My parser has executed Order 66");
     }
 }
